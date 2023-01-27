393f9232ea09e8aeaf1cb12daf0cc497c76b6140
Vincent Ang <vinceajy93@gmail.com>
2023-01-27 10:22:10 +0800

    regex worked

--- task01/src/Main.java
+++ task01/src/Main.java
@@ -1,11 +1,37 @@
 package sg.edu.nus.iss.task01.src;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        //term-frequency formula: freq word/counts of word in document
+
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         String fileName = args[0];
         String dirPath = System.getProperty("user.dir");
+        File file = new File(dirPath + File.separator + fileName);
+
+        String readString, strippedString;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        Integer wordCount = 0;
+        Map<String,Integer> topWords = new HashMap<String,Integer>();
+        try{
+            while((readString = br.readLine()) != null){
+                strippedString = readString.replaceAll("\\p{Punct}", "");
+                wordCount = strippedString.split(" ").length;
+                System.out.println(wordCount);
+                System.out.println(strippedString);
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }finally{
+            br.close();
+        }
+
 
     }
 }
