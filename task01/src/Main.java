dd01cff64e8687d41aa5dc2200f894edb264a0f2
Vincent Ang <vinceajy93@gmail.com>
2023-01-27 09:42:19 +0800

    added text file and get user directory

---
+++ task01/src/Main.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.iss.task01.src;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //term-frequency formula: freq word/counts of word in document
+        String fileName = args[0];
+        String dirPath = System.getProperty("user.dir");
+
+    }
+}
