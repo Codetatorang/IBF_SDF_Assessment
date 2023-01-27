1407b2c18f63ed6534b6be8ffdd82b400e9c0383
Vincent Ang <vinceajy93@gmail.com>
2023-01-27 12:35:22 +0800

    added frequency calculations and print out results

--- task01/src/Main.java
+++ task01/src/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -16,11 +17,12 @@ public class Main{
     public static void main(String[] args) throws FileNotFoundException, IOException{
         String fileName = args[0];
         String dirPath = System.getProperty("user.dir");
-        File file = new File(dirPath + File.separator + fileName);
+        File file = new File(dirPath + File.separator + fileName + ".txt");
 
         String readString, strippedString;
         BufferedReader br = new BufferedReader(new FileReader(file));
-        Integer freq = 0;
+        Integer counts = 0;
+        Integer totalWordCount = 0;
         Map<String,Integer> wordCounts = new LinkedHashMap<String,Integer>();
         try{
             while((readString = br.readLine()) != null){
@@ -28,15 +30,16 @@ public class Main{
                 strippedString = readString.replaceAll("\\p{Punct}", "");
                 String[] wordArray = strippedString.split(" ");
 
+                totalWordCount += strippedString.split(" ").length;
                 for(String word:wordArray){
-                    freq = wordCounts.get(word);
-                    if(freq == null){
-                        freq = 1;
+                    counts = wordCounts.get(word);
+                    if(counts == null){
+                        counts = 1;
                     }
                     else{
-                        freq +=1;
+                        counts +=1;
                     }
-                    wordCounts.put(word, freq);
+                    wordCounts.put(word, counts);
                 }
             }
 
@@ -45,9 +48,10 @@ public class Main{
            //sort through a list
            Collections.sort(entrySetList,(map1,map2) -> map2.getValue().compareTo(map1.getValue()));
 
-            //print out top 10 values 
+            //print out top 10 values as well as frequency
             for(int i =0; i <10; i++){
-                System.out.println("word: '" + entrySetList.get(i).getKey() + "', with number of counts Counts: " + entrySetList.get(i).getValue());
+                Float termFreq = ((float)entrySetList.get(i).getValue() / totalWordCount);
+                System.out.printf("word: '%s', occurs '%d' with counts of %.3f \n", entrySetList.get(i).getKey(),entrySetList.get(i).getValue(), termFreq);
             }
 
         }catch(IOException ex){
